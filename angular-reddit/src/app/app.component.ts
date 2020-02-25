b54f1e9c1e9ff5d1204ac4eb03159b48c84cd0a4
thuannd <thuannd@arrowhitech.co>
2020-02-25 17:03:22 +0700

    thuannd -- 20200325

---
+++ angular-reddit/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { Article } from './article/article.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+  title = 'angular-reddit';
+  articles: Article[];
+
+  constructor(){
+    this.articles = [
+      new Article('Angular','http://angular.io',10),
+      new Article('Fullstack','http://fullstack.io',10),
+      new Article('Angular HomePage','http://angular.io',10)
+    ];
+  }
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement){
+    console.log(`Adding article title: ${title.value} 
+    and link: ${link.value}`);
+    return false;
+  }
+}
